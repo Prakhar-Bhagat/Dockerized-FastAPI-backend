0122c195296e30351287a79be890bc887199bc81
Prakhar-Bhagat <133053719+Prakhar-Bhagat@users.noreply.github.com>
2026-01-24 19:05:04 +0530

    Import status from fastapi in posts router

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from app import schemas, models, database, auth
+from fastapi import status
+
 
 router = APIRouter(
     prefix="/posts",
